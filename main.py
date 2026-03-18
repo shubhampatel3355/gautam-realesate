b68907a52ee3f76f89c5577f582cad85479174a0
shubhampatel3355 <shu7bhampatel3355@gmail.com>
2026-03-18 15:45:32 +0530

    domain3

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ app.add_middleware(
         "http://127.0.0.1:3000",
         "http://127.0.0.1:8000",
         "https://bgmrealestate.in",
+        "https://dashboard.bgmrealestate.in",
     ],
     allow_credentials=True,
     allow_methods=["*"],
